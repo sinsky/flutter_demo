c0194f2b06e243db4266e9e4b0e4291e281986e3
sinsky <60662802+sinsky@users.noreply.github.com>
2024-05-28 17:53:23 +0900

    refactor: add semantics label to BigCard for accessibility

--- lib/main.dart
+++ lib/main.dart
@@ -78,6 +78,7 @@ class BigCard extends StatelessWidget {
         child: Text(
           pair.asLowerCase,
           style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
         ),
       ),
     );
