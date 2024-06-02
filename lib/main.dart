c25b50d064c60727c2f05b91140ce32a7442dd1f
sinsky <60662802+sinsky@users.noreply.github.com>
2024-06-02 11:40:57 +0900

    feat: refactor MyHomePage to use StatefulWidget and manage page selection

--- lib/main.dart
+++ lib/main.dart
@@ -44,9 +44,27 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
     return Scaffold(
       body: Row(
         children: [
@@ -61,15 +79,17 @@ class MyHomePage extends StatelessWidget {
                 label: Text('Favorites'),
               ),
             ],
-            selectedIndex: 0,
+            selectedIndex: selectedIndex,
             onDestinationSelected: (value) {
-              print('selected: $value');
+              setState(() {
+                selectedIndex = value;
+              });
             },
           )),
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
+              child: page,
             ),
           )
         ],
